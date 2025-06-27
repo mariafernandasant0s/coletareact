0d40fb551cc57e5e051a4b15eb1abdc091fc1565
mariahfassina <20233017592@estudantes.ifpr.edu.br>
2025-06-27 13:13:04 -0300

    Update NotFoundPage.js

--- src/pages/public/NotFoundPage.js
+++ src/pages/public/NotFoundPage.js
@@ -3,22 +3,26 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
 
-function NotFoundPage() { // Renomeie o componente também
+
+function NotFoundPage() {
     return (
         <>
             <Helmet>
                 <title>Página não encontrada - Coleta Seletiva</title>
                 <meta name="description" content="Página não encontrada." />
             </Helmet>
-            <section className="info-section">
-                <div className="container">
-                    <h2>Erro 404 - Página não encontrada</h2>
-                    <p>A página que você está procurando não existe ou foi removida.</p>
-                    <Link to="/">Voltar para a página inicial</Link>
-                </div>
-            </section>
+            <main>
+                <section className="info-section">
+                    <div className="container">
+                        <h2>Erro 404 - Página não encontrada</h2>
+                        <p>A página que você está procurando não existe ou foi removida.</p>
+                        <Link to="/">Voltar para a página inicial</Link>
+                    </div>
+                </section>
+            </main>
         </>
     );
 }
 
-export default NotFoundPage; // E a exportação
+
+export default NotFoundPage;
