3648ae18dab59cd051faa3fab6d1942e69ca4da5
mariahfassina <20233017592@estudantes.ifpr.edu.br>
2025-07-11 13:47:52 -0300

    Update UtilityBar.js

--- src/components/common/UtilityBar.js
+++ src/components/common/UtilityBar.js
@@ -5,7 +5,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 function UtilityBar() {
-  // REMOVIDAS AS PROPRIEDADES QUE ESTAVAM QUEBRANDO O APP
   return (
     <div className="utility-bar">
       <div className="container">
