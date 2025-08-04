584c38a98b30e9c844c6703c62bf7cb36ae6ef74
mariahfassina <20233017592@estudantes.ifpr.edu.br>
2025-08-04 11:11:43 -0300

    Update AppRoutes.js

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -4,8 +4,7 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import PrivateRoute from './PrivateRoute';
 
-// ✅ CORREÇÃO APLICADA AQUI: Apontando para o arquivo correto 'Home.js'
-import HomePage from '../pages/public/Home'; 
+import HomePage from '../pages/public/Home'; // Apontando para o seu arquivo Home.js
 import GenericPage from '../pages/public/GenericPage';
 import NotFoundPage from '../pages/public/NotFoundPage';
 
@@ -16,7 +15,6 @@ import EditPage from '../pages/admin/EditPage';
 function AppRoutes() {
   return (
     <Routes>
-      {/* --- Rotas Públicas --- */}
       <Route path="/" element={<HomePage />} />
       <Route path="/como-separar/como-fazer" element={<GenericPage slug="como-fazer-separacao" />} />
       <Route path="/como-separar/residuos" element={<GenericPage slug="quais-residuos" />} />
@@ -25,24 +23,12 @@ function AppRoutes() {
       <Route path="/total-coletado" element={<GenericPage slug="total-coletado-grafico" />} />
       <Route path="/contato" element={<GenericPage slug="contato" />} />
       
-      {/* --- Rotas de Admin --- */}
       <Route path="/login" element={<Navigate to="/admin/login" replace />} />
       <Route path="/admin" element={<Navigate to="/admin/login" replace />} />
-
       <Route path="/admin/login" element={<AdminLogin />} />
+      <Route path="/admin/dashboard" element={<PrivateRoute><AdminDashboard /></PrivateRoute>} />
+      <Route path="/admin/paginas/edit/:id" element={<PrivateRoute><EditPage /></PrivateRoute>} />
 
-      <Route path="/admin/dashboard" element={
-        <PrivateRoute>
-          <AdminDashboard />
-        </PrivateRoute>
-      } />
-      <Route path="/admin/paginas/edit/:id" element={
-        <PrivateRoute>
-          <EditPage />
-        </PrivateRoute>
-      } />
-
-      {/* --- Rota de Erro 404 --- */}
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
