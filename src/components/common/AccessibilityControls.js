2433a463efa383578463eb978fb2602df49ad2ae
mariahfassina <20233017592@estudantes.ifpr.edu.br>
2025-08-04 11:13:12 -0300

    Update AccessibilityControls.js

--- src/components/common/AccessibilityControls.js
+++ src/components/common/AccessibilityControls.js
@@ -1,28 +1,18 @@
 // src/components/common/AccessibilityControls.js
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUniversalAccess } from '@fortawesome/free-solid-svg-icons';
 
-function AccessibilityControls() {
-  const [fontSize, setFontSize] = useState(100); // em porcentagem
-
-  useEffect(() => {
-    document.body.style.fontSize = `${fontSize}%`;
-  }, [fontSize]);
-
-  const aumentarFonte = () => {
-    if (fontSize < 150) setFontSize(fontSize + 10);
-  };
-
-  const diminuirFonte = () => {
-    if (fontSize > 70) setFontSize(fontSize - 10);
-  };
-
+function AccessibilityControls({ onIncreaseFontSize, onDecreaseFontSize }) {
   return (
     <div className="accessibility-controls">
-      <button onClick={diminuirFonte} aria-label="Diminuir fonte">A-</button>
-      <button onClick={aumentarFonte} aria-label="Aumentar fonte">A+</button>
+      <button onClick={onDecreaseFontSize} aria-label="Diminuir tamanho da fonte" title="Diminuir Fonte">
+        A-
+      </button>
+      <button onClick={onIncreaseFontSize} aria-label="Aumentar tamanho da fonte" title="Aumentar Fonte">
+        A+
+      </button>
       <a href="#accessibility-info" aria-label="Acessibilidade">
         <FontAwesomeIcon icon={faUniversalAccess} />
       </a>
