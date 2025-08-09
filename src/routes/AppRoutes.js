0a2ec17020ca2c0dbefd3621be57bdaba451a3aa
mariahfassina <20233017592@estudantes.ifpr.edu.br>
2025-08-09 01:01:22 -0300

    Update AppRoutes.js

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -4,13 +4,14 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import PrivateRoute from './PrivateRoute';
 
-import HomePage from '../pages/public/Home'; // Apontando para o seu arquivo Home.js
+import HomePage from '../pages/public/Home';
 import GenericPage from '../pages/public/GenericPage';
 import NotFoundPage from '../pages/public/NotFoundPage';
 
 import AdminLogin from '../pages/admin/AdminLogin';
 import AdminDashboard from '../pages/admin/AdminDashboard';
 import EditPage from '../pages/admin/EditPage';
+import ChangePasswordPage from '../pages/admin/ChangePasswordPage'; // <-- Importe o novo componente
 
 function AppRoutes() {
   return (
@@ -26,8 +27,11 @@ function AppRoutes() {
       <Route path="/login" element={<Navigate to="/admin/login" replace />} />
       <Route path="/admin" element={<Navigate to="/admin/login" replace />} />
       <Route path="/admin/login" element={<AdminLogin />} />
+      
+      {/* Rotas Protegidas */}
       <Route path="/admin/dashboard" element={<PrivateRoute><AdminDashboard /></PrivateRoute>} />
       <Route path="/admin/paginas/edit/:id" element={<PrivateRoute><EditPage /></PrivateRoute>} />
+      <Route path="/admin/change-password" element={<PrivateRoute><ChangePasswordPage /></PrivateRoute>} /> {/* <-- Nova Rota */}
 
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
