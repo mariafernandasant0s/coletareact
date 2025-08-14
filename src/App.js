204ec348313f6002efbacd88ea6be0e03078c27d
mariahfassina <20233017592@estudantes.ifpr.edu.br>
2025-08-14 02:31:53 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
@@ -7,9 +6,10 @@ import AppRoutes from './routes/AppRoutes';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 
-// 1. IMPORTE O NOVO COMPONENTE
+// Importa o componente do botão "Voltar ao Topo"
 import BackToTopButton from './components/BackToTopButton';
 
+// Importa os arquivos de estilo globais
 import './App.css';
 import "yet-another-react-lightbox/styles.css";
 
@@ -18,16 +18,24 @@ function App() {
     <Router>
       <HelmetProvider>
         <AuthProvider>
+          {/* 
+            Este é o container principal que tem a regra de CSS 
+            que estava escondendo o botão.
+          */}
           <div className="app-container">
             <Header />
             <main className="main-content">
               <AppRoutes />
             </main>
             <Footer />
-            
-            {/* 2. ADICIONE O BOTÃO AQUI */}
-            <BackToTopButton />
           </div>
+          
+          {/* 
+            O BOTÃO FOI MOVIDO PARA CÁ!
+            Estando fora do 'app-container', ele não será mais afetado 
+            pela regra 'overflow' e aparecerá corretamente.
+          */}
+          <BackToTopButton />
         </AuthProvider>
       </HelmetProvider>
     </Router>
