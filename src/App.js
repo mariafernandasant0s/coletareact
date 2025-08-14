2365e5db4a5f299f76e4594a78b2ffa93739fce6
mariafernandasant0s <20233017583@estudantes.ifpr.edu.br>
2025-08-14 12:14:35 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from 'react'; // Apenas o React é necessário aqui
 import { HelmetProvider } from 'react-helmet-async';
-import { AuthProvider } from './contexts/AuthContext';
 import AppRoutes from './routes/AppRoutes';
+
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
+import StickyInstaButton from './components/common/StickyInstaButton';
+import UtilityBar from './components/common/UtilityBar';
 import BackToTopButton from './components/BackToTopButton';
 
-import './App.css';
-import "yet-another-react-lightbox/styles.css";
+import './assets/css/style.css'; 
 
 function App() {
+  // O código 'useEffect' para rolar a página foi removido daqui.
+
   return (
-    <Router>
-      <HelmetProvider>
-        <AuthProvider>
-          <div className="app-container">
-            <Header />
-            <main className="main-content">
-              <AppRoutes />
-            </main>
-            <Footer />
-          </div>
-          
-          {/* Chamar o botão aqui agora é seguro, pois ele será "transportado" para o <body> */}
-          <BackToTopButton />
-        </AuthProvider>
-      </HelmetProvider>
-    </Router>
+    <HelmetProvider>
+      <UtilityBar />
+      <Header />
+      <main>
+        <AppRoutes />
+      </main>
+      <Footer />
+      <StickyInstaButton />
+      <BackToTopButton /> 
+    </HelmetProvider>
   );
 }
 
