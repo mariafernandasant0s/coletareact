4a07790637a3c557b37dccf6b46a7095af352eb7
mariafernandasant0s <20233017583@estudantes.ifpr.edu.br>
2025-08-18 23:20:16 -0300

    Update GenericPage.js

--- src/pages/public/GenericPage.js
+++ src/pages/public/GenericPage.js
@@ -52,7 +52,7 @@ const QuemSomosContent = () => {
       <h4 className="subtitulo-centralizado">Localização UVR</h4>
       <div className="map-container">
         {/* URL DO MAPA ATUALIZADA AQUI */}
-       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3633.7922928942185!2d-53.478903499999994!3d-24.388527999999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94f3a9a0df9b73f3%3A0x1a27a21825729844!2sUVR%20-%20Unidade%20de%20Valoriza%C3%A7%C3%A3o%20de%20Recicl%C3%A1veis%20de%20Assis%20Chateaubriand!5e0!3m2!1spt-BR!2sbr!4v1755569389724!5m2!1spt-BR!2sbr" width="600" height="450" style={{border:0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+       <iframe src="https://www.google.com/maps/place/UVR+-+Unidade+de+Valoriza%C3%A7%C3%A3o+de+Recicl%C3%A1veis+de+Assis+Chateaubriand/data=!4m2!3m1!1s0x0:0x1a27a21825729844?sa=X&ved=1t:2428&ictx=111&cshid=1755569847689513" width="100%" height="450" style={{ border: 0 }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" title="Localização da ACAMAR no Google Maps"></iframe>
       </div>
       </div>
     </div>
@@ -224,5 +224,6 @@ function GenericPage({ slug }) {
 }
 
 export default GenericPage;
+
 
 
