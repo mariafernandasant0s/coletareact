1f60af3089aa2a3c324f1f038fe183ae515f34fa
mariafernandasant0s <20233017583@estudantes.ifpr.edu.br>
2025-09-16 10:09:26 -0300

    Update useOnClickOutside.js

--- src/hooks/useOnClickOutside.js
+++ src/hooks/useOnClickOutside.js
@@ -5,11 +5,11 @@ import { useEffect } from 'react';
 function useOnClickOutside(ref, handler) {
   useEffect(() => {
     const listener = (event) => {
-      // Se o clique for dentro do menu (ref.current) ou em um elemento que o contém, não faz nada
+
       if (!ref.current || ref.current.contains(event.target)) {
         return;
       }
-      // Se o clique for fora, chama a função handler (que será a nossa função de fechar o menu)
+
       handler(event);
     };
 
@@ -17,12 +17,13 @@ function useOnClickOutside(ref, handler) {
     document.addEventListener('mousedown', listener);
     document.addEventListener('touchstart', listener);
 
-    // Função de limpeza: remove os listeners quando o componente for desmontado
+
     return () => {
       document.removeEventListener('mousedown', listener);
       document.removeEventListener('touchstart', listener);
     };
-  }, [ref, handler]); // Roda o efeito novamente se a referência ou o handler mudarem
+  }, [ref, handler]); 
 }
 
 export default useOnClickOutside;
+
