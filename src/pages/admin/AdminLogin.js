518ff757caa59f29951a8de4de62a4d0de4be5c6
mariafernandasant0s <20233017583@estudantes.ifpr.edu.br>
2025-09-16 10:11:52 -0300

    Update AdminLogin.js

--- src/pages/admin/AdminLogin.js
+++ src/pages/admin/AdminLogin.js
@@ -4,7 +4,7 @@ import { Helmet } from 'react-helmet-async';
 import { useAuth } from '../../contexts/AuthContext';
 import './Admin.css';
 
-// Ícones para mostrar/ocultar senha
+
 const EyeOpenIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
     <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
@@ -108,3 +108,4 @@ function AdminLogin() {
 }
 
 export default AdminLogin;
+
