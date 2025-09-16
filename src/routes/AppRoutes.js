2d6f8369253de491c1468e5bd01ec227fdc8789d
mariafernandasant0s <20233017583@estudantes.ifpr.edu.br>
2025-09-16 10:17:11 -0300

    Update AppRoutes.js

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -3,7 +3,6 @@
 import React, { useEffect } from 'react';
 import { Routes, Route, Navigate, useLocation } from 'react-router-dom'; 
 
-// Importações das suas páginas
 import PrivateRoute from './PrivateRoute';
 import HomePage from '../pages/public/Home';
 import GenericPage from '../pages/public/GenericPage';
@@ -12,7 +11,7 @@ import AdminLogin from '../pages/admin/AdminLogin';
 import AdminDashboard from '../pages/admin/AdminDashboard';
 import EditPage from '../pages/admin/EditPage';
 
-// Componente que garante que cada nova página carregue no topo
+
 const ScrollToTop = () => {
   const { pathname, hash } = useLocation();
 
@@ -25,17 +24,15 @@ const ScrollToTop = () => {
   return null;
 };
 
-// Componente principal de rotas
 function AppRoutes() {
   const location = useLocation();
 
   return (
     <>
-      <ScrollToTop /> {/* <-- O ScrollToTop VEM PARA CÁ */}
+      <ScrollToTop /> 
       
-      {/* A 'key' aqui garante que a animação de surgimento rode a cada mudança de página */}
+ 
       <Routes location={location} key={location.pathname}>
-        {/* Rotas Públicas */}
         <Route path="/" element={<HomePage />} />
         <Route path="/como-separar/como-fazer" element={<GenericPage slug="como-fazer-separacao" />} />
         <Route path="/como-separar/residuos" element={<GenericPage slug="quais-residuos" />} />
@@ -45,8 +42,6 @@ function AppRoutes() {
         <Route path="/contato" element={<GenericPage slug="contato" />} />
         <Route path="/faq" element={<GenericPage slug="faq" />} />
         <Route path="/desenvolvedoras" element={<GenericPage slug="desenvolvedoras" />} />
-        
-        {/* Rotas de Admin */}
         <Route path="/login" element={<Navigate to="/admin/login" replace />} />
         <Route path="/admin" element={<Navigate to="/admin/login" replace />} />
         <Route path="/admin/login" element={<AdminLogin />} />
@@ -58,8 +53,6 @@ function AppRoutes() {
           path="/admin/paginas/edit/:id" 
           element={<PrivateRoute><EditPage /></PrivateRoute>} 
         />
-
-        {/* Rota para Página Não Encontrada */}
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
@@ -67,3 +60,4 @@ function AppRoutes() {
 }
 
 export default AppRoutes;
+
